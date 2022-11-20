2f2cecb23985a67c2b211b01501a9b9f2b3c766c
favasquezj <37709118+favasquezj@users.noreply.github.com>
2022-11-19 23:47:09 -0500

    PUNTO 3

---
+++ Principal/src/main/java/Figuras/FiguraGeometrica.java
@@ -0,0 +1,24 @@
+
+package Figuras;
+
+/**
+ *
+ * @author avj10
+ */
+public class FiguraGeometrica {
+    private double volumen;
+    private double superficie;
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+    }
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+    public double getVolumen() {
+        return this.volumen;
+    }
+
+    public double getSuperficie() {
+        return this.superficie;
+    }
+}
---
+++ Principal/src/main/java/Figuras/Principal.java
@@ -0,0 +1,21 @@
+
+package Figuras;
+
+/**
+ *
+ * @author avj10
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        
+        miVentanaPrincipal = new VentanaPrincipal();
+        
+        miVentanaPrincipal.setVisible(true);
+        
+        miVentanaPrincipal.setResizable(false);
+    }
+}
+    
+
